4d04b72ead5b93437b4555121064aa8e062ccb4a
Vexumi <gleb.gaag@gmail.com>
2024-08-10 15:41:53 +0700

    added usersecrets, registered dbcontext

--- CollegeTracker.WEB/Program.cs
+++ CollegeTracker.WEB/Program.cs
@@ -1,17 +1,25 @@
-using Microsoft.AspNetCore.Authentication.Negotiate;
+using System.Reflection;
+
+using CollegeTracker.DataAccess;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Configuration
+    .AddJsonFile("appsettings.json")
+    .AddUserSecrets(Assembly.GetExecutingAssembly(), true);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<CollegeTrackerDbContext>(options => options.UseNpgsql(connectionString));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
