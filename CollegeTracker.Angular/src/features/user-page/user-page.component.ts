09e49dfe828ab7fc3e82c2d4e9629bd12d76f59e
@Gleb.Gaag <gleb.gaag@icl-services.com>
2024-12-26 21:06:55 +0700

    added frontend project

---
+++ CollegeTracker.Angular/src/features/user-page/user-page.component.ts
@@ -0,0 +1,17 @@
+import { NgClass } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-user-page',
+  templateUrl: './user-page.component.html',
+  styleUrls: ['./user-page.component.scss'],
+  imports: [NgClass]
+})
+export class UserPageComponent {
+  protected isSidebarHidden = false;
+
+  toggleSidebar(): void {
+    this.isSidebarHidden = !this.isSidebarHidden;
+  }
+}
