384a893b15f37e712ce69eae7b254a55b0ec3d90
@Gleb.Gaag <gleb.gaag@icl-services.com>
2025-01-05 16:07:47 +0700

    added roll up side menu button

--- CollegeTracker.Angular/src/app/app.component.ts
+++ CollegeTracker.Angular/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../features/website-structure/header/header.component';
+import { SideMenuComponent } from '../features/website-structure/side-menu/side-menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, SideMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ CollegeTracker.Angular/src/features/website-structure/side-menu/side-menu.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-side-menu',
+  templateUrl: './side-menu.component.html',
+  styleUrls: ['./side-menu.component.scss'],
+  imports: []
+})
+export class SideMenuComponent {
+
+  public sideMenuRolledUp = false;
+
+  public onSideMenuClick(): void {
+    this.sideMenuRolledUp = !this.sideMenuRolledUp;
+  }
+}
